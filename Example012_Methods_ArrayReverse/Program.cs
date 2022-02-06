ccf342a16a634352cb66857552f4503e14068994
Akuloff.M <Akuloff.M@gmail.com>
2022-02-06 17:56:34 +0300

    example 012 methods array reverse project

---
+++ Example012_Methods_ArrayReverse/Program.cs
@@ -0,0 +1,35 @@
+﻿// Сортировка одномерного массива методом выбора от min к max
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSotr(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSotr(arr);
+
+PrintArray(arr);
