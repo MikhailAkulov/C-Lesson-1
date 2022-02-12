304a8140187f1b5f8d848c4981815bdc67d1b56a
Akuloff.M <Akuloff.M@gmail.com>
2022-02-12 21:36:46 +0300

    add Example013 Recursion Algorithm_3 Factorial project

---
+++ Example013_RecursionAlgorithm_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Рекурсия на примере вычисления факториала
+
+double Factorial(int n) // double используется потому что тип int не в мещает в себя необходимое количество знаков
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
